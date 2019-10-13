7766e88bada467bc8e4289048c1b0c30775b4b64
Rob Ranf <robranf@gmail.com>
2019-10-12 21:02:56 -0700

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Rob Ranf on 10/12/19.
+//  Copyright © 2019 Rob Ranf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
